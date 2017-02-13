968ffbd0b11b37ceebf3783bbd8145452090cd0e
Mike <mikemekker2@gmail.com>
2017-02-13 11:47:32 -0500

    Created the main menu and game screens

--- Stratego/core/src/com/game/screens/GameScreen.java
+++ Stratego/core/src/com/game/screens/GameScreen.java
@@ -1,98 +1,117 @@
 package com.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.game.stratego.Stratego;
 
 public class GameScreen implements Screen, InputProcessor {
-
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-
+	Stratego game;
+	ShapeRenderer sr;
+	
+	public GameScreen(Stratego game) {
+		Gdx.input.setInputProcessor(this);
+		this.game = game;
+		sr = new ShapeRenderer();
 	}
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		sr.begin(ShapeType.Filled);
+		//Draw top left menu button
+		sr.setColor(Color.LIGHT_GRAY);
+		sr.rect(0, (Gdx.graphics.getHeight()-27), 40, 27);
+		sr.setColor(Color.GRAY);
+		sr.rect(10, (Gdx.graphics.getHeight()-10), 20, 4);
+		sr.rect(10, (Gdx.graphics.getHeight()-15), 20, 4);
+		sr.rect(10, (Gdx.graphics.getHeight()-20), 20, 4);
+		//Draw board
+		sr.setColor(Color.WHITE);
+		sr.rect((Gdx.graphics.getWidth()/2)-250, (Gdx.graphics.getHeight()/2)-250, 501, 502);
+		sr.setColor(Color.BLACK);
+		for(int x = 0; x < 10; x++) {
+			for(int y = 10; y > 0; y--) {
+				sr.rect(((Gdx.graphics.getWidth()/2)-250)+(x+1)+(49*x), ((Gdx.graphics.getHeight()/2)-250)+(y+1)+(49*(y-1)), 49, 49);
+			}
+		}
+		//Draw trays
+		sr.setColor(Color.WHITE);
+		sr.rect(20, 15, 165, 488);
+		sr.setColor(Color.BLACK);
+		for(int x = 0; x < 2; x++) {
+			for(int y = 6; y > 0; y--) {
+				sr.rect((21)+(x+1)+(80*x), (15)+(y+1)+(80*(y-1)), 80, 80);
+			}
+		}
+		sr.setColor(Color.WHITE);
+		sr.rect(775, 15, 165, 488);
+		sr.setColor(Color.BLACK);
+		for(int x = 0; x < 2; x++) {
+			for(int y = 6; y > 0; y--) {
+				sr.rect((776)+(x+1)+(80*x), (15)+(y+1)+(80*(y-1)), 80, 80);
+			}
+		}
+		sr.end();
+	}
+	
+	@Override
+	public void show() {
 
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	public boolean keyDown(int keycode) {return false;}
 	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	public boolean keyUp(int keycode) {return false;}
 	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	public boolean keyTyped(char character) {return false;}
 	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {return false;}
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
+		//int newScreenY = (Gdx.graphics.getHeight()) - screenY;
+		if(screenX > 0 && screenX < 40
+				&& screenY > 0 && screenY < 25) {
+			game.setScreen(new MainMenu(game));
+		}
 		return false;
 	}
-
 	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	public boolean touchDragged(int screenX, int screenY, int pointer) {return false;}
 	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	public boolean mouseMoved(int screenX, int screenY) {return false;}
 	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean scrolled(int amount) {return false;}
 
 }
--- Stratego/core/src/com/game/stratego/Stratego.java
+++ Stratego/core/src/com/game/stratego/Stratego.java
@@ -1,12 +1,9 @@
 package com.game.stratego;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.game.screens.MainMenu;
 
 public class Stratego extends Game {
-	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
@@ -20,10 +17,5 @@ public class Stratego extends Game {
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-	}
-	
-	public void changeScreen(Screen screen){
-		this.setScreen(screen);
 	}
 }
