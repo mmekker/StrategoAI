c1129559f495a33835e32aca000343c52d138ebd
mmekker <mikemekker2@gmail.com>
2017-04-20 18:36:25 -0400

    Fixed it

--- Stratego/core/src/main/java/com/game/stratego/core/Stratego.java
+++ Stratego/core/src/main/java/com/game/stratego/core/Stratego.java
@@ -2,18 +2,11 @@ package com.game.stratego.core;
 
 import com.badlogic.gdx.Game;
 import com.game.stratego.core.screens.MainMenu;
-import com.game.stratego.core.stratego.Match;
 
 public class Stratego extends Game {
 	@Override
 	public void create () {
-		boolean train = true;
-		if(!train) {
-			this.setScreen(new MainMenu(this));
-		}
-		else if(train) {
-			Match.main(null);
-		}
+		this.setScreen(new MainMenu(this));
 	}
 
 	@Override
--- Stratego/desktop/src/main/java/com/game/stratego/java/StrategoDesktop.java
+++ Stratego/desktop/src/main/java/com/game/stratego/java/StrategoDesktop.java
@@ -4,12 +4,19 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import com.game.stratego.core.Stratego;
+import com.game.stratego.core.stratego.Match;
 
 public class StrategoDesktop {
 	public static void main (String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Stratego(), config);
-		config.width = 960;
-		config.height = 620;
+		boolean train = true;
+		if(train) {
+			Match.main(null);
+		}
+		else {
+			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+			new LwjglApplication(new Stratego(), config);
+			config.width = 960;
+			config.height = 620;
+		}
 	}
 }
